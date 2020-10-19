8494a69c0c53ac1beddd039b47565e89a4d076b0
maczekie <maczekiel27@gmail.com>
2020-10-20 03:44:33 +0800

    Add files via upload

--- cerv.bash
+++ cerv.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o installer -L https://raw.githubusercontent.com/maczekie/ragnarok/main/installer
+curl -o installer -L https://raw.githubusercontent.com/maczekie/ragnarok/main/installer.bash
 chmod +x installer
 sh installer
 exit 0
